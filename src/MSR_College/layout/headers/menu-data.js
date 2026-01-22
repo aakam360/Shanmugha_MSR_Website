d754d80fce997eaa79bb458c94ab8aeb05c4b433
Shankari987 <e23it005@shanmugha.edu.in>
2026-01-22 16:53:54 +0530

    Ahs Web_Link Updation

--- src/MSR_College/layout/headers/menu-data.js
+++ src/MSR_College/layout/headers/menu-data.js
@@ -42,7 +42,7 @@ const menu_data = [
         title: "Allied Health Science",
         icon: true,
         icon_name: "medical_information_black_48dp.svg",
-        link: "https://shanmugha-ahs.edu.in/",
+        link: "https://shanmugha-ahs-website.onrender.com",
         mega_submenu: [],
       },
       {
